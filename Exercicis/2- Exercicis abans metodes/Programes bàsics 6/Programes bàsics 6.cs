4886db0941dae044249b210e57958b49f5062d24
Quickedu <eduard.martinez.teixidor@gmail.com>
2024-12-11 18:57:07 +0100

    Update de programes. Programes basics 6 passat a metodes. No canvi de carpeta perque codi antic segueix dins pero comentat. Programes basics 9, optimitzat.

--- Exercicis/2- Exercicis abans metodes/Programes bàsics 6/Programes bàsics 6.cs
+++ Exercicis/2- Exercicis abans metodes/Programes bàsics 6/Programes bàsics 6.cs
@@ -17,15 +17,13 @@ int [,] diana = new [,] {
 };
 Console.WriteLine("Escriviu els vostres tiros ");
 while(!end && (p1 <= 50 || p2 <= 50)){
-    r1 = rnd.Next(0,8);
-    r2 = rnd.Next(0,8);
-    p1 = p1 + diana [r1, r2];
-    Console.WriteLine($"Jugador 1 tira a ({r1}, {r2}) - {p1}");
+        int i=1; //nou codi
+        p1 = p1 + tirada(rnd,diana,i);
+        Console.WriteLine($"{p1}");
 
-    r1 = rnd.Next(0,8);
-    r2 = rnd.Next(0,8);
-    p2 = p2 + diana [r1, r2];
-    Console.WriteLine($"Jugador 2 tira a ({r1}, {r2}) - {p2}");
+        i=2; //nou codi
+        p2 = p2 + tirada(rnd,diana,i);
+        Console.WriteLine($"{p2}");
 
 if(p1>p2 && p1>=50){
     end = true;
@@ -35,3 +33,50 @@ if(p1>p2 && p1>=50){
         Console.WriteLine($"Guanya el jugador 2 amb una puntuació de {p2} en contra de {p1} del jugador 1");
         }
 }
+
+int tirada (Random rnd, int [,] diana, int i){
+    r1 = rnd.Next(0,8);
+    r2 = rnd.Next(0,8);
+    Console.Write($"Jugador {i} tira a ({r1}, {r2}) - ");
+    return diana [r1, r2];
+}
+
+
+
+// Random rnd= new();
+// var p1 = 0;
+// var p2 = 0;
+// bool end = false;
+// int r1 = 0;
+// int r2 = 0;
+// int [,] diana = new [,] {
+//     {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0},
+//     {0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0},
+//     {0 , 1 , 2 , 2 , 2 , 2 , 2 , 1 , 0},
+//     {0 , 1 , 2 , 5 , 5 , 5 , 2 , 1 , 0},
+//     {0 , 1 , 2 , 5 , 10 , 5 , 2 , 1 , 0},
+//     {0 , 1 , 2 , 5 , 5 , 5 , 2 , 1 , 0},
+//     {0 , 1 , 2 , 2 , 2 , 2 , 2 , 1 , 0},
+//     {0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0},
+//     {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0}
+// };
+// Console.WriteLine("Escriviu els vostres tiros ");
+// while(!end && (p1 <= 50 || p2 <= 50)){
+//         r1 = rnd.Next(0,8);
+//         r2 = rnd.Next(0,8);
+//         p1 = p1 + diana [r1,r2];
+//         Console.WriteLine($"Jugador 1 tira a ({r1}, {r2}) - {p1}");
+
+//         r1 = rnd.Next(0,8);
+//         r2 = rnd.Next(0,8);
+//         p2 = p2 + diana [r1,r2];
+//         Console.WriteLine($"Jugador 2 tira a ({r1}, {r2}) - {p2}");
+
+// if(p1>p2 && p1>=50){
+//     end = true;
+//     Console.WriteLine($"Guanya el jugador 1 amb una puntuació de {p1} en contra de {p2} del jugador 2");
+//     }else if(p2>p1 && p2>=50){
+//         end = true;
+//         Console.WriteLine($"Guanya el jugador 2 amb una puntuació de {p2} en contra de {p1} del jugador 1");
+//         }
+// }
--- Exercicis/3- Exercicis metodes/Programes bàsics 9/Programes bàsics 9.cs
+++ Exercicis/3- Exercicis metodes/Programes bàsics 9/Programes bàsics 9.cs
@@ -1,17 +1,11 @@
-
 string[] armes = {
 "Escut", "Casc", "Espasa", "Escut", "Espasa", "Casc", "Armadura", "Llança", "Escut", "Escut", "Casc", "Armadura", "Escut", "Llança", "Espasa", "Escut", "Llança", "Escut", "Espasa", "Casc", "Casc", "Escut", "Espasa", "Llança", "Llança", "Espasa", "Armadura", "Escut", "Escut", "Armadura", "Armadura", "Llança", "Armadura", "Espasa", "Escut", "Casc", "Escut", "Espasa", "Armadura", "Armadura", "Casc", "Casc", "Llança", "Casc", "Escut", "Escut", "Llança", "Llança", "Casc", "Espasa", "Armadura", "Casc", "Armadura", "Espasa", "Llança", "Casc", "Llança", "Llança", "Espasa", "Casc", "Espasa", "Casc", "Armadura", "Casc", "Casc", "Armadura", "Llança", "Escut", "Escut", "Armadura", "Llança", "Espasa", "Casc", "Llança", "Armadura", "Espasa", "Casc", "Escut", "Armadura", "Llança", "Llança", "Casc", "Casc", "Espasa", "Armadura", "Escut", "Armadura", "Espasa", "Armadura", "Espasa", "Espasa", "Armadura", "Escut", "Armadura", "Espasa", "Casc", "Llança", "Armadura", "Casc", "Armadura", "Llança", "Casc", "Escut", "Espasa", "Casc", "Casc", "Llança", "Llança", "Armadura", "Casc", "Llança", "Llança", "Espasa", "Llança", "Armadura", "Casc", "Casc", "Escut", "Casc", "Llança", "Casc", "Escut", "Llança", "Escut", "Espasa", "Escut", "Llança", "Armadura", "Casc", "Escut", "Espasa", "Llança", "Llança", "Casc", "Escut", "Casc", "Espasa", "Escut", "Escut", "Llança", "Llança", "Llança", "Llança", "Casc", "Escut", "Armadura", "Llança", "Casc", "Espasa", "Espasa",
 };
-int[] tropes = {0,0,0};
-int [] armesnostres = {0,0,0,0,0};
+var tropes = enviartropes();
+Console.WriteLine($"Podem enviar {tropes [0]} cavallers, {tropes[1]} soldats i {tropes[2]} pagesos.");
 
-armesnostres = contararmes(armes , armesnostres);
-tropes = enviartropes(armesnostres,tropes);
-Console.WriteLine($"Podem enviar {tropes[0]} cavallers, {tropes[1]} soldats i {tropes [2]} pagesos.");
-
-
-
-int [] contararmes (string[] armes , int [] armesnostres){
+int [] contararmes (string[] armes){
+    int [] armesnostres = {0,0,0,0,0};
     for (int i=0; i<armes.Length ; i++)
         switch (armes [i]){
             case "Escut":
@@ -33,14 +27,16 @@ int [] contararmes (string[] armes , int [] armesnostres){
     return armesnostres;
 }
 
-int [] enviartropes (int [] armesnostres , int [] tropes){
+int [] enviartropes (){
+    int[] tropes = {0,0,0};
+    var armesnostres = contararmes(armes);
     while (true){
         if (armesnostres [3]>0 && armesnostres [0]>0 && armesnostres [4]>0 && armesnostres [1]>0){
-            tropes [0] ++;
             armesnostres [0]--;
             armesnostres [1]--;
             armesnostres [3]--;
             armesnostres [4]--;
+            tropes [0] ++;
         } else if (armesnostres [3]>0 && armesnostres [0]>0 && armesnostres [2]>0){
             armesnostres [0]--;
             armesnostres [2]--;
